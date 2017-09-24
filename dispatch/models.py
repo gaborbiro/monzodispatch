f2bb19b076634297841442c9fd772a48e69006b1
Gabor Biro <gabor.biro@yahoo.com>
2017-09-24 19:36:41 +0100

    Added created and updated fields to the MonzoToken table

--- dispatch/models.py
+++ dispatch/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 class MonzoToken(models.Model):
     hash = models.CharField(max_length=50, null=False, blank=False, unique=True)
     token = models.CharField(max_length=200, null=False, blank=False, unique=True)
+    added = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.name or self.reference
