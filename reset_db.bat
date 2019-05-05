9515805a25728374600c4b14d4470a347b286828
Gabor Biro <gabor.biro@yahoo.com>
2019-05-05 19:01:40 +0100

    Updated scripts

--- reset_db.bat
+++ reset_db.bat
@@ -1,7 +1,11 @@
-del db.sqlite3
-del monzodispatch\migrations\*
+REM Unix/Linux only!
 
+REM delete database and migration files
+del db.sqlite3
+del     monzodispatch\migrations\*
+
+REM regenerate migration files and database based on current models
 python manage.py makemigrations monzodispatch
 python manage.py migrate
 
-REM python manage.py createsuperuser --username admin@monosaur.com --email admin@monosaur.com
+python manage.py createsuperuser --username admin@canibuyit.com --email admin@canibuyit.com
